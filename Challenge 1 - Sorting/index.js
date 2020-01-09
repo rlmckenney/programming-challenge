8b56749521ac6e02b22b235617121dfddc282dd3
Robert McKenney <robert@mckenney.ca>
2020-01-09 10:01:16 -0500

    add solution for challenge 1

---
+++ Challenge 1 - Sorting/index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/**
+ * Returns a new array sorted from lowest to hightest.
+ *
+ * @param {Number[]} source
+ * @returns {Number[]}
+ */
+function sortNumbers (source) {
+  return source.reduce((sorted, el) => {
+    if (typeof el === 'number') {
+      let index = 0
+      while (index < sorted.length && sorted[index] < el) index++
+      sorted.splice(index, 0, el)
+    }
+    return sorted
+  }, [])
+}
+
+let randomIntegers = Array.from(
+  { length: 100 },
+  () => Math.floor(Math.random() * 99) + 1
+)
+
+console.log(sortNumbers(randomIntegers))
